f28b34d024f8ca7f1d27a6354c4dd38ad885aed8
arkanorun <arkanorun@gmail.com>
2024-02-29 20:21:23 -0300

    atualização em vaidação de cpf na endpoin de create e update na pasta patient

--- backend/src/controllers/patient/Update.js
+++ backend/src/controllers/patient/Update.js
@@ -1,3 +1,4 @@
+const validate = require("cpf-rg-validator")
 const knex = require("../../models/connection")
 
 const patientUpdate = async (req, res) => {
@@ -10,6 +11,7 @@ const patientUpdate = async (req, res) => {
 
         if (!nome && !telefone && !endereco && !cidade && !cpf && !rg && !convenio &&
             !codigo_carteira_saude && !medico_principal && !data_nascimento) {
+
             return res.status(400).json({
                 mensagem: "Informe ao menos um campo para ser atualizado"
             })
@@ -25,11 +27,36 @@ const patientUpdate = async (req, res) => {
 
         if (cpf) {
 
+            function validateRegexCPF(cpf) {
+                let validateCpf = /^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}/
+                if (cpf.match(validateCpf)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            if (!validateRegexCPF(cpf)) {
+
+                return res.status(400).json({
+                    mensagem: "insira um cpf no formato XXX.XXX.XXX-XX"
+                })
+
+            }
+
+            const cpfvalidate = validate.cpf(cpf)
+
+            if (!cpfvalidate) {
+                return res.status(400).json({
+                    mensagem: "insira um cpf válido no formto XXX.XXX.XXX-XX"
+                })
+            }
+
             const searchPatientCpf = await knex('pacientes')
                 .where({ cpf })
                 .first()
 
-            if (searchPatientCpf) {
+            if (searchPatientCpf && searchPatient.cpf != cpf) {
                 return res.status(400).json({
                     mensagem: 'já existe paciente cadastrado com o cpf informado'
                 })
@@ -37,6 +64,15 @@ const patientUpdate = async (req, res) => {
         }
 
         if (rg) {
+
+            const rgvalidate = validate.rg(rg)
+
+            if (!rgvalidate) {
+                return res.status(400).json({
+                    mensagem:
+                        "insira um rg válido"
+                })
+            }
 
             const searchPatientRg = await knex('pacientes')
                 .where({ rg })
@@ -69,6 +105,7 @@ const patientUpdate = async (req, res) => {
         return res.json(updadePacient)
 
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ mensagem: error.message })
     }
 }
