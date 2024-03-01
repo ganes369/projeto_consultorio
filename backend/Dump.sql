ba884ad20e02fd2c521703ada8fa37ea81cd1fee
arkanorun <arkanorun@gmail.com>
2024-03-01 17:21:12 -0300

    atualização do Dump.sql e exclusão das bibliotecas de validação de cpf-rg e formatação de valores

--- backend/Dump.sql
+++ backend/Dump.sql
@@ -4,8 +4,8 @@ create table pacientes (
   telefone text not null,
   endereco text not null,
   cidade text not null,
-  CPF integer not null unique,
-  RG integer not null unique,
+  CPF varchar(14) not null unique,
+  RG varchar(20) not null unique,
   convenio text,
   codigo_carteira_saude text,
   medico_principal text not null,
@@ -14,9 +14,17 @@ create table pacientes (
 
   create table convenios (
   id serial primary key,
-  Nome text not null,
-  Convenio text not null,
-  Codigo_Referenciado text,
-  Usuario integer not null,
-  Senha text not null
+  convenio text not null,
+  valor_consulta text not null,
+  tipo_cobranca text not null,
+  profissional_1 text,
+  profisional_2 text,
+  profissional_3 text,
+  profissional_4 text,
+  profissional_5 text,
+  profissional_6 text,
+  profissional_7 text,
+  codigo_referenciado text,
+  Usuario text,
+  Senha text
   )
